16b4427e0dada51bca23fcc23cef47c520ecd285
Odai Atef <oalbaky@aidm.ai>
2021-01-04 22:09:22 +0300

    following task done

--- managmentlayer/browserAction/script.js
+++ managmentlayer/browserAction/script.js
@@ -1,5 +1,88 @@
-console.log("Plugin Loaded");
+var API = "http://35.159.18.195/api";
+var handler = null;
+browser.storage.onChanged.addListener(newEvent);
 
+
+function newEvent(data) {
+    checkHandler();
+    updateDone(data);
+    // document.getElementById("newEvent").innerHTML = "New Event";
+}
+
+function updateDone(data) {
+    if (data['done_tasks'] != undefined) {
+        try {
+            var xhttp = new XMLHttpRequest();
+            var requestd_data = {"account_handler": handler, "id": data['done_tasks'].newValue.id};
+
+            xhttp.open("POST", API + "/tasks/executed", true);
+            xhttp.setRequestHeader("Content-Type", "application/json");
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var response = JSON.parse(this.responseText);
+                    if (response['status'] != undefined && response['status'] =="1") {
+                        browser.storage.local.get().then((cached_data) => {
+                            delete cached_data['done_tasks'];
+                            delete cached_data['tasks']
+                            browser.storage.local.set(cached_data);
+                        });
+                    }
+                    document.getElementById("result").innerHTML = response['id'];
+                } else {
+                    document.getElementById("result").innerHTML = "Fail to get the tasks" + JSON.parse(requestd_data);
+                }
+            };
+            xhttp.send(JSON.stringify(requestd_data));
+        } catch (e) {
+            document.getElementById("result").innerHTML = JSON.stringify(e);
+        }
+    }
+}
+
+function checkHandler() {
+    browser.storage.local.get().then((d) => {
+        if (d['handler'] != undefined) {
+            handler = d['handler'];
+            document.getElementById("handler").innerHTML = handler;
+        }
+    });
+}
+
+function checkTasks() {
+    if (handler != null) {
+        try {
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("POST", API + "/tasks/index", true);
+            xhttp.setRequestHeader("Content-Type", "application/json");
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var response = JSON.parse(this.responseText);
+                    if (response['id'] != undefined) {
+                        browser.storage.local.get().then((cached_data) => {
+                            cached_data['tasks'] = response;
+                            browser.storage.local.set(cached_data);
+                        });
+                    }
+                    document.getElementById("result").innerHTML = response['id'];
+                } else {
+                    document.getElementById("result").innerHTML = "Fail to get the tasks" + this.status;
+                }
+            };
+            var data = {"account_handler": handler};
+            xhttp.send(JSON.stringify(data));
+        } catch (e) {
+            document.getElementById("result").innerHTML = JSON.stringify(e);
+        }
+    }
+}
+
+checkHandler();
+checkTasks();
+
+setInterval(() => {
+    checkHandler();
+    checkTasks();
+}, 10000);
 
 // function followAccount() {
 //     console.log("Clicked");
--- managmentlayer/content_script.js
+++ managmentlayer/content_script.js
@@ -1,54 +1,64 @@
 // Put all the javascript code here, that you want to execute after page load.
 console.log("Odai plugin has been loaded ");
-var openedAt = new Date();
-var API="http://35.159.18.195/api";
-// setTimeout(function () {
-//     createColumn();
-// }, 10000)
+var API = "http://35.159.18.195/api";
+var handler = null;
 
 function getHandler() {
-    handler = null;
     try {
-        var handler = document.getElementsByClassName("js-account-summary")[0].getElementsByTagName('a')[0].getAttribute("data-user-name");
+        var new_handler = document.getElementsByClassName("js-account-summary")[0].getElementsByTagName('a')[0].getAttribute("data-user-name");
+        if (new_handler && handler != new_handler) {
+            browser.storage.local.set({
+                "handler": new_handler
+            }).then(() => {
+            });
+        }
     } catch (e) {
 
     }
-    return handler;
+}
+
+browser.storage.onChanged.addListener(new_storage);
+
+function new_storage(data) {
+    console.log(data);
+    if (data['tasks'] != undefined) {
+        if (data['tasks'].newValue.type == 'follow') {
+            window.open("https://www.twitter.com/" + data['tasks'].newValue.data);
+
+        }
+    }
 }
 
 function createColumn() {
-    document.getElementsByClassName("js-app-search-input app-search-input search-input")[0].value="السعودية  OR ksa";
+    document.getElementsByClassName("js-app-search-input app-search-input search-input")[0].value = "السعودية  OR ksa";
     document.getElementsByClassName("js-perform-search txt-size--14 search-input-perform-search")[0].click()
 }
 
 function followAccount() {
     var ele = document.getElementsByClassName("css-18t94o4 css-1dbjc4n r-1niwhzg r-p1n3y5 r-sdzlij r-1phboty r-rs99b7 r-1w2pmg r-1vuscfd r-1dhvaqw r-1ny4l3l r-1fneopy r-o7ynqc r-6416eg r-lrvibr");
-    ele[0].click();
+    if (ele[0] !== undefined) {
+        ele[0].click();
+        browser.storage.local.get().then((cached_data) => {
+            var current_user = window.location.href.replace("https://www.twitter.com/","").replace("https://twitter.com/","") ;
+            if (current_user == cached_data['tasks'].data) {
+                var done_tasks = cached_data['tasks'];
+                cached_data['done_tasks'] = done_tasks;
+                browser.storage.local.set(cached_data).then((d)=>{
+                    setTimeout(() => {
+                        window.close();
+                    }, 1000);
+                });
+            }
+        });
+
+    }
 }
 
+//Always check for handlers
+setTimeout(() => {
+    getHandler();
+    followAccount();
+}, 3000)
 setInterval(function () {
-    var handler = getHandler();
-    if (handler != null) {
-        try {
-            var xhttp = new XMLHttpRequest();
-            console.log(API+"/tasks/index");
-            xhttp.open("POST", API+"/tasks/index", true);
-            xhttp.setRequestHeader("Content-Type", "application/json");
-            xhttp.onreadystatechange = function () {
-                console.log(this.responseText);
-                if (this.readyState == 4 && this.status == 200) {
-                    var response = this.responseText;
-                    console.log(response);
-                } else {
-                    // window.open("https://twitter.com","_blank");
-                    console.log("Fail to get the tasks");
-                }
-            };
-            var data = {account_handler: handler, started_at: openedAt.getTime()};
-            console.log(data);
-            xhttp.send(JSON.stringify(data));
-        } catch (e) {
-            console.log(e)
-        }
-    }
-}, 5000);
+    getHandler();
+}, 60000);
