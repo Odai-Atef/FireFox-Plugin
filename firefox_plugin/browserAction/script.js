2625f35ca437695a748bd87c2e8d0187a863b3fe
Odai Atef <oalbaky@aidm.ai>
2021-01-06 15:08:00 +0300

    fixing bugs

--- firefox_plugin/browserAction/script.js
+++ firefox_plugin/browserAction/script.js
@@ -1,61 +1,60 @@
 var API = "http://35.159.18.195/api";
 var handler = null;
+
 browser.storage.onChanged.addListener(newEvent);
-
-checkHandler();
-checkTasks();
-
-setInterval(() => {
-    checkHandler();
-    checkTasks();
-}, 10000);
+var number_of_requests = 0;
 
 //functions
 function newEvent(data) {
-    checkHandler();
-    updateDone(data);
-    // document.getElementById("newEvent").innerHTML = "New Event";
-}
-function updateDone(data) {
-    if (data['done_tasks'] != undefined) {
-        try {
-            var xhttp = new XMLHttpRequest();
-            var requestd_data = {"account_handler": handler, "id": data['done_tasks'].newValue.id};
-
-            xhttp.open("POST", API + "/tasks/executed", true);
-            xhttp.setRequestHeader("Content-Type", "application/json");
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    var response = JSON.parse(this.responseText);
-                    if (response['status'] != undefined && response['status'] =="1") {
-                        browser.storage.local.get().then((cached_data) => {
-                            delete cached_data['done_tasks'];
-                            delete cached_data['tasks']
-                            browser.storage.local.set(cached_data);
-                        });
-                    }
-                    document.getElementById("result").innerHTML = response['id'];
-                } else {
-                    document.getElementById("result").innerHTML = "Fail to get the tasks" + JSON.parse(requestd_data);
-                }
-            };
-            xhttp.send(JSON.stringify(requestd_data));
-        } catch (e) {
-            document.getElementById("result").innerHTML = JSON.stringify(e);
-        }
+    if (data.event.newValue == "EVENT_HANDLER_FETCHED") {
+        checkHandler(data);
+        checkTasks();
+    }
+    if (data.event.newValue == "EVENT_TASK_DONE") {
+        checkDone(data);
     }
 }
-function checkHandler() {
-    browser.storage.local.get().then((d) => {
-        if (d['handler'] != undefined) {
-            handler = d['handler'];
-            document.getElementById("handler").innerHTML = handler;
-        }
-    });
+
+function checkDone(data) {
+
+    try {
+        var xhttp = new XMLHttpRequest();
+        var requestd_data = {"account_handler": handler, "id": data.tasks.newValue.id};
+        xhttp.open("POST", API + "/tasks/executed", true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = JSON.parse(this.responseText);
+                document.getElementById("result").innerHTML = "Fail to get the tasks" + this.responseText;
+                if (response['status'] != undefined && response['status'] == "1") {
+                    browser.storage.local.get().then((cached_data) => {
+                        delete cached_data['tasks'];
+                        cached_data['event']="EVENT_HANDLER_FETCHED";
+                        browser.storage.local.set(cached_data);
+                    });
+                }
+                document.getElementById("result").innerHTML = response['id'];
+            } else {
+                document.getElementById("result").innerHTML = "Fail to get the tasks" + JSON.stringify(requestd_data);
+            }
+        };
+        xhttp.send(JSON.stringify(requestd_data));
+    } catch (e) {
+        document.getElementById("result").innerHTML = JSON.stringify(e);
+    }
+
 }
+
+function checkHandler(data) {
+    handler = data.handler.newValue;
+    document.getElementById("handler").innerHTML = "@" + handler;
+}
+
 function checkTasks() {
     if (handler != null) {
         try {
+            number_of_requests = number_of_requests + 1;
+            document.getElementById("requests").innerHTML = "" + number_of_requests;
             var xhttp = new XMLHttpRequest();
             xhttp.open("POST", API + "/tasks/index", true);
             xhttp.setRequestHeader("Content-Type", "application/json");
@@ -65,6 +64,7 @@ function checkTasks() {
                     if (response['id'] != undefined) {
                         browser.storage.local.get().then((cached_data) => {
                             cached_data['tasks'] = response;
+                            cached_data['event'] = "EVENT_TASK_READY";
                             browser.storage.local.set(cached_data);
                         });
                     }
--- firefox_plugin/content_script.js
+++ firefox_plugin/content_script.js
@@ -1,23 +1,28 @@
 var handler = null;
-browser.storage.onChanged.addListener(newStorage);
+var last_event_time = Date.now();
+browser.storage.onChanged.addListener(newEvent);
 //Always check for handlers
 setTimeout(() => {
     getHandler();
     followAccount();
 }, 3000)
-setInterval(function () {
-    getHandler();
-}, 60000);
 
+setInterval(() => {
+    if ((Date.now() - last_event_time) / 1000 > 30) {
+        getHandler();
+    }
+
+}, 10000);
 
 //functions
 function getHandler() {
     try {
         var new_handler = document.getElementsByClassName("js-account-summary")[0].getElementsByTagName('a')[0].getAttribute("data-user-name");
         if (new_handler && handler != new_handler) {
-            browser.storage.local.set({
-                "handler": new_handler
-            }).then(() => {
+            browser.storage.local.get().then((cached_data) => {
+                cached_data["handler"] = new_handler;
+                cached_data["event"] = "EVENT_HANDLER_FETCHED";
+                browser.storage.local.set(cached_data);
             });
         }
     } catch (e) {
@@ -25,39 +30,45 @@ function getHandler() {
     }
 }
 
-function newStorage(data) {
-    if (data['tasks'] != undefined) {
-        if (data['tasks'].newValue.type == 'follow') {
-            window.open("https://www.twitter.com/" + data['tasks'].newValue.data);
-        } else if (data['tasks'].newValue.type == 'rule') {
-            createColumn(data['tasks'].newValue.data);
+function newEvent(data) {
+    console.log("new storage", data);
+    last_event_time = Date.now();
+    if (data['tasks'] != undefined && data.event.newValue == "EVENT_TASK_READY") {
+        if (data.tasks.newValue.type == 'follow') {
+            window.open("https://www.twitter.com/" + data.tasks.newValue.data);
+        } else if (data.tasks.newValue.type == 'rule') {
+            createColumn(data.tasks.newValue.data);
         }
     }
 }
 
 function createColumn(data) {
-    document.getElementsByClassName("js-app-search-input app-search-input search-input")[0].value = data;
-    document.getElementsByClassName("js-perform-search txt-size--14 search-input-perform-search")[0].click()
+    if (document.getElementsByClassName("js-perform-search txt-size--14 search-input-perform-search")[0] != undefined) {
+        document.getElementsByClassName("js-app-search-input app-search-input search-input")[0].value = data;
+        document.getElementsByClassName("js-perform-search txt-size--14 search-input-perform-search")[0].click();
+        browser.storage.local.get().then((cached_data) => {
+            cached_data['event'] = "EVENT_TASK_DONE";
+            browser.storage.local.set(cached_data);
+        });
+    }
 }
 
 function followAccount() {
     var ele = document.getElementsByClassName("css-18t94o4 css-1dbjc4n r-1niwhzg r-p1n3y5 r-sdzlij r-1phboty r-rs99b7 r-1w2pmg r-1vuscfd r-1dhvaqw r-1ny4l3l r-1fneopy r-o7ynqc r-6416eg r-lrvibr");
     if (ele[0] !== undefined) {
         ele[0].click();
-        browser.storage.local.get().then((cached_data) => {
-            var current_user = window.location.href.replace("https://www.twitter.com/", "").replace("https://twitter.com/", "");
-            if (current_user == cached_data['tasks'].data) {
-                var done_tasks = cached_data['tasks'];
-                cached_data['done_tasks'] = done_tasks;
-                browser.storage.local.set(cached_data).then((d) => {
-                    setTimeout(() => {
-                        window.close();
-                    }, 1000);
-                });
-            }
-        });
-
     }
+    browser.storage.local.get().then((cached_data) => {
+        var current_user = window.location.href.replace("https://www.twitter.com/", "").replace("https://twitter.com/", "");
+        if (current_user == cached_data['tasks'].data) {
+            cached_data['event'] = "EVENT_TASK_DONE";
+            browser.storage.local.set(cached_data).then((d) => {
+                setTimeout(() => {
+                    window.close();
+                }, 1000);
+            });
+        }
+    });
 }
 
 console.log("DMMIL has been loaded");
